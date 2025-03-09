860aa4c84854298a4a22a10e7b551c103d820ba3
Abdi Rahman Suufi <93520190+Abdi-Suufi@users.noreply.github.com>
2025-03-09 05:45:30 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -50,4 +50,5 @@ wss.on("connection", (ws) => {
 
 app.get("/", (req, res) => res.send("Connect 4 Backend Running!"));
 
-server.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
